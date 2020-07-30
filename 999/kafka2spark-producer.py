a7a9ba7d07c6c77c18d1c766630f83f35e9db1bb
sshia1 <sshia1@outlook.com>
2020-07-30 17:50:24 -0400

    update

--- 999/kafka2spark-producer.py
+++ 999/kafka2spark-producer.py
@@ -13,7 +13,7 @@ pObj			= KafkaProducer(bootstrap_servers=bootstrap_servers, value_serializer=lam
 url			= "https://covid-193.p.rapidapi.com/statistics"
 headers		= {
 				'x-rapidapi-host': "covid-193.p.rapidapi.com",
-				'x-rapidapi-key': "b7b5f2745dmshabc69c1dcba13f2p191da7jsnd98306ea16c5"
+				'x-rapidapi-key': ""
 }
 
 
--- 999/read-api-reformat.py
+++ 999/read-api-reformat.py
@@ -7,7 +7,7 @@ import json
 url		= "https://covid-193.p.rapidapi.com/statistics"
 headers	= {
 	'x-rapidapi-host': "covid-193.p.rapidapi.com",
-	'x-rapidapi-key': "b7b5f2745dmshabc69c1dcba13f2p191da7jsnd98306ea16c5",
+	'x-rapidapi-key': "",
 	"format":"json"
 }
 
